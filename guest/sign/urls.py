872a28cfaf0e6b503397ad0d9747adaa63468402
wewendeng <dengweiwen@163.com>
2019-02-24 21:51:59 +0800

    添加加密接口路径

--- guest/sign/urls.py
+++ guest/sign/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
+
+import views_sec_api
 from sign import views_api
+from sign.views_sec_api import get_event_list, add_event, get_guest_list
 
 
 urlpatterns = [
-    path('get_event_list/', views_api.get_event_list, name='get_event_list'),
+    path('get_event_list/', views_api.get_event_list),
     path('add_event/', views_api.add_event),
     path('get_guest_list/', views_api.get_guest_list),
     path('add_guest/', views_api.add_guest),
-    path('user_sign/', views_api.user_sign)
+    path('user_sign/', views_api.user_sign),
+    path('sec_get_event_list/', views_sec_api.get_event_list,),
+    path('sec_add_event/', views_sec_api.add_event,),
+    path('get_guest_list', views_sec_api.get_guest_list),
 ]
