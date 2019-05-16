1fa0d9ca332a0608bfdb801f24511bf4da1fc349
wewendeng <wewendeng@outlook.com>
2019-05-16 14:49:24 +0800

    添加

--- flask/simple_tools/get_count.py
+++ flask/simple_tools/get_count.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 str = input("请输入要统计的单词：")
- 
+
 res = {}
 for i in str:
     if i in res:
@@ -16,3 +16,23 @@ d = defaultdict(int)
 for k in s:
   d[k] += 1
 print(d)
+
+
+with open('./data.txt', 'r') as datas:
+    result = {}
+    for line in datas.readlines():
+        data = line.strip().split('/')
+        for i in data:
+            if i in result:
+                result[i] = result[i] + 1
+            else:
+                result[i] = 1
+    print(result)
+
+# 99乘法表
+for i in range(1,10):
+    for j in range(1, i+1):
+        result = i * j
+        print("{0} * {1} = {2}\t".format(j, i, result), end='')
+    print()
+
